c58a65884ded5f504f1823c3cf879801fe993de3
Volodymyr <woland31252@gmail.com>
2024-09-28 21:53:26 +0300

    formating files

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,12 @@
-import { Suspense } from 'react';
-import Navigation from '../Navigation/Navigation.jsx';
-import css from '../Layout/Layout.module.css';
+import { Suspense } from "react";
+import Navigation from "../Navigation/Navigation.jsx";
+import css from "../Layout/Layout.module.css";
 
-export default function Layout({children}) {
-    return (
-        <div className={css.container}>
-            <Navigation />
-            <Suspense fallback={<p>Loading page...</p>}>{children}</Suspense> 
-        </div>
-    )
+export default function Layout({ children }) {
+  return (
+    <div className={css.container}>
+      <Navigation />
+      <Suspense fallback={<p>Loading page...</p>}>{children}</Suspense>
+    </div>
+  );
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,17 +1,20 @@
 import { NavLink } from "react-router-dom";
-import css from './Navigation.module.css';
-import clsx from 'clsx';
+import css from "./Navigation.module.css";
+import clsx from "clsx";
 
 export default function Navigation() {
+  const buildLinkClass = ({ isActive }) => {
+    return clsx(css.link, isActive && css.active);
+  };
 
-    const buildLinkClass = ({ isActive }) => {
-        return clsx(css.link, isActive && css.active);
-    };
-    
-    return (
-        <nav className={css.link}>
-        <NavLink to="/" className={buildLinkClass}>Home Page</NavLink>
-        <NavLink to="/movies" className={buildLinkClass}>Movies Page</NavLink>
-      </nav>
-    )
+  return (
+    <nav className={css.link}>
+      <NavLink to="/" className={buildLinkClass}>
+        Home Page
+      </NavLink>
+      <NavLink to="/movies" className={buildLinkClass}>
+        Movies Page
+      </NavLink>
+    </nav>
+  );
 }
